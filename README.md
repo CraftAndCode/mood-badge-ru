15b3890b49ca6d9be1b15acb250c73aac7543e1e
CraftAndCode <89014755+CraftAndCode@users.noreply.github.com>
2021-09-14 22:23:29 +0300

    Update pxt.json, main.blocks, main.ts, README.md

--- README.md
+++ README.md
@@ -1,31 +1,38 @@
+### @autoOpen = true
 
-> Open this page at [https://craftandcode.github.io/mood-badge-ru/](https://craftandcode.github.io/mood-badge-ru/)
+#### 1. Подключите ваш Micro:bit с помощью кабеля USB.
+#### 2. Синхронизируйте Micro:bit, нажав кнопку `||led....||` - `||led.Подключить устройство||`.
+#### 3. Загрузите программу умного бэйджа, нажав `||led.Скачать||`.
+#### 4. Узнайте больше о программировании, запустив интерактивные уроки.
 
-## Использовать это расширение
+## Учимся программировать
 
-This repository can be added as an **extension** in MakeCode.
+```codecard
+[
+{
+    "name": "2.1 Мигаем светодиодами",
+    "imageUrl":"https://raw.githubusercontent.com/CraftAndCode/mood-badge/master/mood%20badge%20schematic.gif",
+    "url": "https://makecode.microbit.org/#tutorial:github:craftandcode/flashing-leds/ru", 
+    "description": "Learn to flash external LEDs", 
+    "cardType": "tutorial",
+    "label": "Easy",
+    "labelClass": "green ribbon small"
+}
+]
+```
 
-* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
-* click on **New Project**
-* click on **Extensions** under the gearwheel menu
-* search for **https://github.com/craftandcode/mood-badge-ru** and import
-
-## Править этот проект ![Build status badge](https://github.com/craftandcode/mood-badge-ru/workflows/MakeCode/badge.svg)
-
-To edit this repository in MakeCode.
-
-* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
-* click on **Import** then click on **Import URL**
-* paste **https://github.com/craftandcode/mood-badge-ru** and click import
-
-## Blocks preview
-
-This image shows the blocks code from the last commit in master.
-This image may take a few minutes to refresh.
-
-![A rendered view of the blocks](https://github.com/craftandcode/mood-badge-ru/raw/master/.github/makecode/blocks.png)
-
-#### Metadata (used for search, rendering)
-
-* for PXT/microbit
-<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
+## Умный бэйдж
+В этом уроке вы дополните программу умного бэйджа.
+```codecard
+[
+{
+    "name": "Как вас зовут?",
+    "imageUrl":"https://raw.githubusercontent.com/CraftAndCode/mood-badge/master/mood%20badge%20schematic.gif",
+    "url": "https://makecode.microbit.org/#tutorial:github:craftandcode/mood-badge/ru", 
+    "description": "Научите бэйдж показывать настроение, имя и время", 
+    "cardType": "tutorial",
+    "label": "Advanced",
+    "labelClass": "orange ribbon small"
+}
+]
+```
--- main.ts
+++ main.ts
@@ -1,3 +1,87 @@
+input.onButtonPressed(Button.A, function () {
+    Mode = "Mood"
+    if (mood == List_of_moods.length - 1) {
+        mood = 0
+    } else {
+        mood += 1
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    Mode = "Heart"
+})
+input.onButtonPressed(Button.B, function () {
+    Mode = "Name"
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    Mode = "Watch"
+})
+input.onGesture(Gesture.Shake, function () {
+    Mode = "Oracle"
+})
+let mood = 0
+let Mode = ""
+let List_of_moods: Image[] = []
+let name = "Your name"
+timeanddate.setTime(11, 30, 0, timeanddate.MornNight.AM)
+List_of_moods = [
+images.iconImage(IconNames.Silly),
+images.iconImage(IconNames.Happy),
+images.iconImage(IconNames.Sad),
+images.iconImage(IconNames.Confused),
+images.iconImage(IconNames.Angry),
+images.iconImage(IconNames.Fabulous),
+images.createImage(`
+    . . # . .
+    . # . # .
+    . . . # .
+    . . # . .
+    . . # . .
+    `)
+]
+Mode = "Heart"
 basic.forever(function () {
-	
+    if (Mode == "Heart") {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.showIcon(IconNames.Heart)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.showIcon(IconNames.SmallHeart)
+    } else if (Mode == "Mood") {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        List_of_moods[mood].showImage(0)
+    } else if (Mode == "Name") {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.clearScreen()
+        proportionalFont.showString("" + name + "   ", 200)
+    } else if (Mode == "Watch") {
+        basic.clearScreen()
+        proportionalFont.showString("" + timeanddate.time(timeanddate.TimeFormat.HMMAMPM) + "   ", 200)
+    } else if (Mode == "Oracle") {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        images.createBigImage(`
+            . . . . # # # # # #
+            . . . # # # # # # #
+            . . # # # # # # # #
+            . # # # # # # # # #
+            # # # # # # # # # #
+            `).scrollImage(1, 200)
+        if (Math.randomBoolean()) {
+            pins.digitalWritePin(DigitalPin.P0, 1)
+            basic.showIcon(IconNames.Happy)
+        } else {
+            if (Math.randomBoolean()) {
+                pins.digitalWritePin(DigitalPin.P1, 1)
+                basic.showIcon(IconNames.Sad)
+            } else {
+                basic.showString("?")
+            }
+        }
+        basic.pause(2000)
+        if (Mode == "Oracle") {
+            Mode = "Heart"
+        }
+    }
 })
